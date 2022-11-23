c8bff76fecef1d3a6cb6af268742221b9eb1156e
lifeinpassion <lifeinpassion@gmail.com>
2022-11-23 07:44:52 +0200

    changes made from lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,17 +8,17 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    path(route='index.html', view=views.index, name='home'),
     # path for about view
-
+    path(route='about.html', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact.html', view=views.contact, name='contact'),
     # path for registration
-
+    path(route='registration/', view=views.registration_request, name='registration'),
     # path for login
-
+    path(route='login/', view=views.login_request, name='login'),
     # path for logout
-
+    path(route='logout/', view=views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
